15e117ca5bc39058b8aef6f48da17596656a3e5c
unknown <kirk.surber@surberphotography.com>
2026-07-18 14:26:54 -0500

    updated audit items

--- src/pages/audits/AuditList.jsx
+++ src/pages/audits/AuditList.jsx
@@ -27,7 +27,7 @@ export default function AuditList() {
   useEffect(() => {
     api.get('/audits').then(setAudits).catch(() => {});
     api.get('/business').then(setBusinesses).catch(() => {});
-    api.get('/business-leads').then(setLeads).catch(() => {});
+    api.get('/leads').then(setLeads).catch(() => {});
   }, []);
 
   function slugify(str) {
